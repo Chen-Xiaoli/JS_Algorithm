7bd829faac15e1a314569cb38641ae71a47b640e
chen xiaoli <34932601+Chen-Xiaoli@users.noreply.github.com>
2024-03-31 11:15:46 +0800

    Update 05_longest_palindromic_substring.js

--- Leetcode/05_longest_palindromic_substring.js
+++ Leetcode/05_longest_palindromic_substring.js
@@ -1,6 +1,38 @@
 //With Brute force algorithm
-// With dynamic programming
-//With O(n^2) complexity. Traverse around the center point to both sides
+// With dynamic programming, O(n^2) time complexity, O(n^2) sapce complexity.
+/*
+State Equation, 
+j-i=0, d[i][j] = 1
+j-i=1, d[i][j] = s[i] == s[j]
+j-i=2, d[i][j] = s[i] == s[j]
+j-i=3, d[i][j] = s[i] == s[j] && d[i+1][j-1]
+*/
+var longestPalindrome = function(s) { 
+  let d = new Array(s.length).fill(false);
+  for(let index=0; index < s.length; index++){
+    d[index] = new Array(s.length).fill(false);
+  }
+  let subStr = '';
+  let mexDis = 0;
+  //呈倒三角开遍历，先里后外
+  for(let i=s.length-1; i>=0; i--) {
+    for(let j=i; j < s.length; j++){
+      d[i][j] = (s[i]==s[j]) && (((j-i)<3) || d[i+1][j-1])
+      if(d[i][j] && mexDis < j-i) {
+        subStr = s.substring(i, j+1);
+        mexDis = j-i;
+      }
+    }
+  }
+  return subStr
+}
+
+//With slide window, O(n^2) time complexity, O(n^2) sapce complexity
+var longestPalindrome = function(s) {
+  
+}
+
+//With O(n^2) time complexity, O(n) sapce complexity. Traverse around the center point to both sides
 var longestPalindrome = function(s) {
   let sn = ['^', '#', ...[...s].join('#'), '#', '$'];
   let antStr = '';
@@ -16,7 +48,7 @@ var longestPalindrome = function(s) {
   return antStr.replaceAll('#', '')
 }
 
-//With O(n) complexity, Manacher's Algorithm
+//With O(n) time complexity, O(n) sapece complexity. Manacher's Algorithm
 //c is center point, and r is right point for the maximum coverage point.
 var longestPalindrome = function(s) {
     let sn = ['^', '#', ...[...s].join('#'), '#', '$'];
