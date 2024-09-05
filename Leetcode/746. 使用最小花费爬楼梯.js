545b6e54e80695358e277124e457593f09a3e8c6
chen xiaoli <34932601+Chen-Xiaoli@users.noreply.github.com>
2024-09-05 21:59:51 +0800

    Create 746._使用最小花费爬楼梯.js

---
+++ Leetcode/746. 使用最小花费爬楼梯.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+
+ time complexity: O(n)
+ space complexity: O(1)
+ */
+var minCostClimbingStairs = function(cost) {
+
+let min = 0;
+let end = cost.length;
+let a = cost[end-1];
+let b = cost[end-2];
+if(cost.length == 2) {
+   min = Math.min(a, b);
+} else {
+    for(let i=end-3; i > 0; i--) {
+    min = Math.min(cost[i] + a, cost[i] + b);
+    a = b;
+    b = min;
+}
+min = Math.min(cost[0] + a, cost[0] + b, b);
+}
+return min;
+};
